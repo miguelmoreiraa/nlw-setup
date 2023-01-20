afa26e910f55f554f91f594f5d791ed0498d636e
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2023-01-19 23:04:46 -0300

    Created events of save date habits

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  book: ["01-03"],
-  takePills: ["01-02"],
+// Events
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  // dayExists === true. Eu fiz a condional desse jeito.
+  // mas a forma que podemos fazer também é não comparar com TRUE...
+  // se for false ele ja cai fora do condição, pois a função por default
+  // é TRUE.
+  if (dayExists) {
+    alert("Esse dia já está registrado 🚫")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
